3d65663d143414ce70bd817b784c343c47fe8ad8
chinhnc <chinhnc@tigren.com>
2024-02-24 19:26:18 +0700

    init env

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const host = process.env.HOST || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port, port, () => {
-    console.log(`Server is running at ${host}:${port}/`)
-  })
+// server.listen(port, port, () => {
+//     console.log(`Server is running at ${host}:${port}/`)
+//   })
+server.listen(port);
